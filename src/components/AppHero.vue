92b9fd22255a936d23459fd7cb8d22d97f7c2a84
Emanuele-Fasciano <emanuelefasciano@gmail.com>
2023-02-24 16:52:15 +0100

    end exercise + bonus

---
+++ src/components/AppHero.vue
@@ -0,0 +1,26 @@
+<script>
+
+</script>
+
+
+<template>
+    <div class="jumbotron">
+        <h2>Il mio primo jumbotron con vite!</h2>
+    </div>
+</template>
+
+
+<style scoped>
+    .jumbotron{
+        width: 100%;
+        height: 60vh;
+        background-image: url(../assets/testimonials_home_1_bg.jpg)
+    }
+
+    h2{
+        text-align: center;
+        color: white;
+        font-weight: 3rem;
+        line-height: 60vh;
+    }
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,16 @@
+<script>
+
+</script>
+
+
+<template>
+<h1>La mia prima app con Vite!</h1>
+</template>
+
+
+<style scoped>
+h1{
+    text-align: center;
+    font-size: 4rem;
+}
+</style>
